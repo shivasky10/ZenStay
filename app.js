3ca313e58b1874268158d8dfdf91f554acb18bb2
shivasky10 <sky10shiva@gmail.com>
2025-07-18 19:54:38 +0530

    backend

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose =require("mongoose");
+const path = require("path");
+const methodOverride=require("method-override");
+
+
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 const mongourl="mongodb://127.0.0.1:27017/zenstay";
 
@@ -16,20 +24,62 @@ main().then(()=>{
     console.log("error");
 })
 
-app.get("/testListing",async (req,res)=>{
-    let sampleListing = new Listing({
-        title : "Villa 101",
-        description : "at city",
-        price : 1500,
-        location : "hyderabad",
-        country : "india",
-    });
+//index route
+app.get("/listings",async(req,res)=>{
+   let allListings = await Listing.find({});
+   res.render("listings/index.ejs",{allListings});
+})
 
-    await sampleListing.save();
-    console.log("saved");
-    res.send("testing  done");
-    
+//newroute
+app.get("/listings/new", async(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+//create route
+app.post("/listings", async (req,res)=>{
+    const newlisting = new Listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
+})
+
+
+
+//show route
+app.get("/listing/:id", async (req,res)=>{
+    let { id }=req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
 });
+
+//edit route
+app.get("/listings/:id/edit",async (req,res)=>{
+    let { id }=req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
+})
+
+//update route
+app.put("/listings/:id",async (req,res)=>{
+    let { id }=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.Listing});
+    res.redirect("/listings");
+})
+
+
+// app.get("/testListing",async (req,res)=>{
+//     let sampleListing = new Listing({
+//         title : "Villa 101",
+//         description : "at city",
+//         price : 1500,
+//         location : "hyderabad",
+//         country : "india",
+//     });
+
+//     await sampleListing.save();
+//     console.log("saved");
+//     res.send("testing  done");
+    
+// });
 
 
 app.get("/",(req,res)=>{
