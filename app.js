78828b076040120d1fafe350f0a44b8ea17a808b
shivasky10 <sky10shiva@gmail.com>
2025-07-20 18:46:49 +0530

    phase 1 styling

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const mongoose =require("mongoose");
 const path = require("path");
 const methodOverride=require("method-override");
+const ejsmate=require("ejs-mate");
 
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
+app.engine("ejs",ejsmate);
+app.use(express.static(path.join(__dirname,"public")));
 
 const mongourl="mongodb://127.0.0.1:27017/zenstay";
 
