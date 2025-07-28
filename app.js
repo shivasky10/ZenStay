0683157c4883bc35cefeef3b4ca1071139093b96
shivasky10 <sky10shiva@gmail.com>
2025-07-28 22:12:28 +0530

    middlewares

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride=require("method-override");
 const ejsmate=require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 
 app.set("view engine","ejs");
@@ -29,6 +30,17 @@ main().then(()=>{
     console.log("error");
 })
 
+const validateListing =(req,res,next)=>{
+    let {error} = listingSchema.validate(req.body);
+    
+    if(error){
+        let errmsg =error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errmsg);
+    }else{
+        next();
+    }
+}
+
 //index route
 app.get("/listings",async(req,res)=>{
    let allListings = await Listing.find({});
@@ -41,10 +53,7 @@ app.get("/listings/new", async(req,res)=>{
 })
 
 //create route
-app.post("/listings", wrapAsync(async (req,res,next)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"send valid data");
-    }
+app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
     const newlisting = new Listing(req.body.listing);
     await newlisting.save();
     res.redirect("/listings");
@@ -68,11 +77,11 @@ app.get("/listings/:id/edit",async (req,res)=>{
 })
 
 //update route
-app.put("/listings/:id",async (req,res)=>{
+app.put("/listings/:id",validateListing, wrapAsync(async (req,res)=>{
     let { id }=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listing/${id}`);
-});
+}));
 
 //delete route
 app.delete("/listings/:id",async (req,res)=>{
