e9eceda2004e5776f336dc1efcc917af33be975c
Shiva Kumar Yadav <160407079+shivasky10@users.noreply.github.com>
2025-08-20 14:21:31 +0530

    maps features

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,10 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: [78.4772, 17.4065], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9 // starting zoom
 });
+
+ const marker = new mapboxgl.Marker({color :"red"})
+        .setLngLat(coordinates)
+        .addTo(map);
